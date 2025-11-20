adb079fda3572cbc081c06734e2559dd19bed6da
Gautham <161573971+Gautham-hash@users.noreply.github.com>
2025-11-20 10:47:57 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
 
         int sum = number1 + number2;
 
-        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+        System.out.println("Sum= "+sum);
     }
 }
